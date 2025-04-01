2c822cedf86e46bf8c81a7c71891238a99704cb8
sailenser <zybov.m@gmail.com>
2025-04-01 20:57:42 +0300

    docker config 28

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Api Laravel</title>
+        <title>Api backend</title>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <p>Main Api Laravel</p>
+        <p>Api backend</p>
     </body>
 </html>
